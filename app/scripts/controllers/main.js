b4cc1ee04a710d68f44b26310b2b3a9ff6cbc704
Tucker Tatum <invalidpleb1@gmail.com>
2016-04-22 20:15:50 -0600

    Added promise to github api

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,24 +8,135 @@
  * Controller of the resumeApp
  */
 angular.module('resumeApp')
-  .controller('MainCtrl', function ($http, $q) {
-
-  	var gitHub = 'https://api.github.com';
-
-  	function githubInfo() {
+	.factory('getNames', function ($http, $q) {
 		var defer = $q.defer();
-		$http.get(gitHub + '/repos/InvalidPleb/Runner-Calculator')
-			.success(function(res) {
-				defer.resolve(res);
-			})
-			.error(function(err, status) {
-				defer.reject(err);
-			});
+	    return {
+	    	get: function (call) {
+				$http.get(call)
+					.success(function(res) {
+						defer.resolve(res);
+					})
+					.error(function(err, status) {
+						defer.reject(err);
+					});
 
-		return defer.promise;
-  	}
+				return defer.promise;
+	  		}
+	    };
+	})
+  	.controller('MainCtrl', function ($http, $q, $scope, getNames) {
 
-  	console.log(githubInfo());
+	  	var i, j;
 
-	    
-  });
+	  	function add(a, b) {
+	      	return a + b;
+	  	}
+
+	  	var githubGet = 'https://api.github.com';
+	  	var reposGet = '/users/InvalidPleb/repos';
+	  	var runCalcCommitGet = '/repos/InvalidPleb/Runner-Calculator/stats/commit_activity';
+	  	var commitContainer = [];
+	  	var repoContainer = [];
+
+	  	function loopCommits(res, container) {
+	  		
+	  		for (i=0; i < res.length; i++) {
+
+	  			if (res[i].total > 0) {
+
+	  				for (j=0; j < res[i].days.length; j++) {
+	  					container.push(res[i].days[j]);
+
+	  				}
+	  			}
+	  		}
+
+	  		return container.reduce(add, 0);
+
+	  	}
+
+	  	function init() {
+	  		getNames.get('https://api.github.com/users/InvalidPleb/repos')
+		  		.then(function(res){
+		  			pushCalledRepo(res);
+		  	});
+			
+	  	}
+
+	  	function getCommits (repo) {
+	  		getNames.get('https://api.github.com/repos/' + repo + '/stats/commit_activity')
+		  		.then(function(res){
+		  			pushCalledRepo(res);
+		  	});
+	  	}
+
+	  	function pushCalledRepo (res) {
+
+	  		for (i = 0; i < res.length; i++) {
+	  			repoContainer.push(res[i].full_name);
+
+	  		}
+	  		
+	  	}
+
+	  	console.log(repoContainer);
+
+
+
+
+
+	  	init();
+
+	  	/*
+
+	  	
+
+	  	githubInfo(githubGet + reposGet).then(function(res){
+
+	  		var fullNames = [];
+
+	  		for (i = 0; i < res.length; i++) {
+	  			fullNames.push(res[i].full_name);
+
+	  		}
+
+	  		return githubInfo(githubGet + '/repos/' + fullNames[0] + '/stats/commit_activity');
+
+
+	  	}).then(function(res){
+
+	  	}).catch(function(err){
+
+	  	});
+
+	  	githubInfo(githubGet + runCalcCommitGet).then(function(res){
+	  		console.log(loopCommits(res, commitContainer));
+
+	  	}).catch(function(err){
+	  		console.log("nop");
+
+	  	});
+
+
+
+	  	/*
+
+	  	var commits;
+
+	  	var promise = $http.get('https://api.github.com/repos/InvalidPleb/Runner-Calculator/stats/commit_activity');
+	  	promise.then(function(payload) {
+	  		return payload.data;
+	  	});
+
+	  	/*
+
+	  	function(a,u){
+	        u.getUser().then(function(user){
+	            a.user = user;
+	            console.log(a.user);
+	        });
+	    }
+	    */
+
+	  	//console.log(commits);
+});
