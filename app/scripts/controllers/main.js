78faed3d307f71166ef0f67fef3faa242fe5d15a
Tucker Tatum <invalidpleb1@gmail.com>
2016-04-24 22:01:38 -0600

    Began adding d3 graph

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -60,6 +60,7 @@ angular.module('resumeApp')
 	  	function getGithubStuff() {
 	  		return $http.get(githubGet + '/users/InvalidPleb/repos')
 		  		.then(function(res){
+		  			console.log(res);
 		  			pushCalledRepo(res.data);
 		  			return getOwnerCommits(repoContainer[0], -1, repoContainer.length);
 		  		});
@@ -104,11 +105,109 @@ angular.module('resumeApp')
 	  		}
 	  	}
 
-	  	var calls = getGithubStuff();
-
+	  	//var calls = getGithubStuff();
+	  	/*
 	  	$q.all([calls]).then(function(){
 	  		console.log(commitOwnerTotal);
 	  	});
+
+		*/
+
+		//Make an SVG Container
+		
+		/*Draw the Circle
+		var circle = svgContainer.append("rect")
+			.attr("x", 200)
+			.attr("y", 30)
+			.attr("width", 300)
+			.attr("height", 300);
+
+		
+
+		var lineData = [
+			{x: 0, y: 0},
+			{x: 100, y: 200},
+			{x: 200, y: 300},
+			{x: 300, y: 350},
+		];
+
+		var line = d3.svg.line()
+			.x(function(d) {return d.x; })
+			.y(function(d) {return d.y; });
+
+		var group = svgContainer.append("g")
+		.attr("transform", "translate(100, 100)");
+
+		group.selectAll("path")
+			.data([lineData])
+			.enter()
+			.append("path")
+			.attr("d", line)
+			.attr("stroke", "#000")
+			.attr("stroke-width", 10);
+		*/
+
+		var svgContainer = d3.select(".animation-container").append("svg")
+			.attr("width", 700)
+			.attr("height", 500);
+
+		function arc (inRad, outRad, sAng, eAng) {
+
+			return d3.svg.arc()
+				.innerRadius(inRad)
+			    .outerRadius(outRad)
+			    .startAngle(sAng * (Math.PI/180))
+			    .endAngle(eAng * (Math.PI/180));
+
+		}
+
+		var counter = 0;
+		var sAngle = -1;
+		var eAngle = 4;
+
+		function outerRing () {
+
+			if (sAngle === -1 && eAngle === 4) {
+
+				sAngle++;
+				eAngle++;
+
+			} else {
+
+				sAngle = sAngle + 5;
+				eAngle = eAngle + 5;
+		
+			}
+
+			counter++;
+
+			if (counter < 52) {
+
+				svgContainer.append("path")
+				    .attr("d", arc(140, 160, sAngle, eAngle))
+				    .attr("transform", "translate(200,200)")
+				    .style("fill", "grey")
+				    .on("mouseover", mouse()).on("mouseout", function (d) {
+					    d3.select(this).style("fill", "black");
+					});
+
+
+				sAngle = sAngle + 2.05;
+				eAngle = eAngle + 2.05;
+
+				return outerRing();
+
+			}
+		}
+
+		function mouse (){
+			console.log("yo");
+
+		}
+
+
+
+		outerRing();
 
 	  	/*
 
