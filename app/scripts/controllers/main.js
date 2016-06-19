854b1fdd14209a4672fa84275a0444c36fe304b0
Tucker Tatum <invalidpleb1@gmail.com>
2016-06-19 17:35:03 -0600

    Made canvas and tools container responsive for medium devices

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,54 +13,6 @@
 	angular.module('resumeApp')
 
 	  	.controller('MainCtrl', function ($http, $q, $scope) {
-
-	  	(function(){
-
-	  		var tag = document.createElement('script');
-
-	      tag.src = "https://www.youtube.com/iframe_api";
-	      var firstScriptTag = document.getElementsByTagName('script')[0];
-	      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-	      // 3. This function creates an <iframe> (and YouTube player)
-	      //    after the API code downloads.
-	      var player;
-	      function onYouTubeIframeAPIReady() {
-	        player = new YT.Player('player', {
-	          height: '390',
-	          width: '640',
-	          videoId: 'M7lc1UVf-VE',
-	          events: {
-	            'onReady': onPlayerReady,
-	            'onStateChange': onPlayerStateChange
-	          }
-	        });
-	      }
-
-	      // 4. The API will call this function when the video player is ready.
-	      function onPlayerReady(event) {
-	        event.target.playVideo();
-	      }
-
-	      // 5. The API calls this function when the player's state changes.
-	      //    The function indicates that when playing a video (state=1),
-	      //    the player should play for six seconds and then stop.
-	      var done = false;
-	      function onPlayerStateChange(event) {
-	        if (event.data == YT.PlayerState.PLAYING && !done) {
-	          setTimeout(stopVideo, 6000);
-	          done = true;
-	        }
-	      }
-	      function stopVideo() {
-	        player.stopVideo();
-	      }
-
-
-
-	  	})();
-	      // 2. This code loads the IFrame Player API code asynchronously.
-	      
 	  		
 
 	  		// -------- Block Template Instance Data -------- //
@@ -576,24 +528,23 @@
 
 			var calls = getGithubStuff();
 		  	$q.all([calls]).then(function(){
-
-		  		(function() {
-			        function hideScreen () {
-			          // Checking if user browser has a firefox only feature
-			          if (typeof InstallTrigger !== 'undefined') {
-			            setTimeout(function() {
-			              $('.loadingScreen').fadeOut(500);
-			              $('.loading').fadeOut(500);
-			            }, 200);
-			          } else {
-			            setTimeout(function() {
-			              $('.loadingScreen').fadeOut(500);
-			              $('.loading').fadeOut(500);
-			            }, 200);
-			          }
-			        }
-			        hideScreen();
-			    })();
+		  		
+		        function hideScreen () {
+		          // Checking if user browser has a firefox only feature
+		          if (typeof InstallTrigger !== 'undefined') {
+		            setTimeout(function() {
+		              $('.loadingScreen').fadeOut(500);
+		              $('.loading').fadeOut(500);
+		            }, 200);
+		          } else {
+		            setTimeout(function() {
+		              $('.loadingScreen').fadeOut(500);
+		              $('.loading').fadeOut(500);
+		            }, 200);
+		          }
+		        }
+		        hideScreen();
+			    
 
 		  		var gotDayCommits = getDayCommits(commitDaily);
 		  		var repoCommits = gotDayCommits[0];
@@ -660,8 +611,8 @@
 
 
 		  	$(window).scroll(function(){
-		  		parallax($('.container-parallax'), 0, -0.4);
-		  		parallax($('.padding'), 0, -0.6);
+		  		//parallax($('.container-parallax'), 0, -0.4);
+		  		parallax($('.padding'), 0, -0.4);
 		  		//parallax($('.decoration'), -0.1, 0);
 		  	});
 
